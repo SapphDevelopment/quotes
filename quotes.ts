00b1509f6fbe5f7278f96879dc769221d266db86
Ducko <micasniebuhr@gmail.com>
2023-06-29 22:36:17 +0200

    Update quotes.ts

--- quotes.ts
+++ quotes.ts
@@ -1,6 +1,9 @@
 import { categories, Quote } from "./interfaces.js";
 
 const quote: Quote[] = [
+  /**
+   * Inspiration Quotes
+   */
   {
     text: "Your time is limited, don't waste it living someone else's life.",
     author: "Steve Jobs",
@@ -13,6 +16,253 @@ const quote: Quote[] = [
     category: categories.inspiration,
     tags: ["imagination", "knowledge"],
   },
+  /**
+   * Motivation Quotes
+   */
+  {
+    text: "When you have a dream, you've got to grab it and never let go.",
+    author: "Carol Burnett",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "Nothing is impossible. The word itself says 'I'm possible!'",
+    author: "Audrey Hepburn",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "There is nothing impossible to they who will try.",
+    author: "Alexander the Great",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "The bad news is time flies. The good news is you're the pilot.",
+    author: "Michael Altshuler",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "Life has got all those twists and turns. You've got to hold on tight and off you go.",
+    author: "Nicole Kidman",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "Keep your face always toward the sunshine, and shadows will fall behind you.",
+    author: "Walt Whitman",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "Be courageous. Challenge orthodoxy. Stand up for what you believe in. When you are in your rocking chair talking to your grandchildren many years from now, be sure you have a good story to tell.",
+    author: "Amal Clooney",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "You make a choice: continue living your life feeling muddled in this abyss of self-misunderstanding, or you find your identity independent of it. You draw your own box.",
+    author: "Duchess Meghan",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "I just want you to know that if you are out there and you are being really hard on yourself right now for something that has happened ... it's normal. That is what is going to happen to you in life. No one gets through unscathed. We are all going to have a few scratches on us. Please be kind to yourselves and stand up for yourself, please.",
+    author: "Taylor Swift",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+    author: "Winston Churchill",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "You define your own life. Don't let other people write your script.",
+    author: "Oprah Winfrey",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "You are never too old to set another goal or to dream a new dream.",
+    author: "Malala Yousafzai",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "At the end of the day, whether or not those people are comfortable with how you're living your life doesn't matter. What matters is whether you're comfortable with it.",
+    author: "Dr. Phil",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "People tell you the world looks a certain way. Parents tell you how to think. Schools tell you how to think. TV. Religion. And then at a certain point, if you're lucky, you realize you can make up your own mind. Nobody sets the rules but you. You can design your own life.",
+    author: "Carrie Ann Moss",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "For me, becoming isn’t about arriving somewhere or achieving a certain aim. I see it instead as forward motion, a means of evolving, a way to reach continuously toward a better self. The journey doesn't end.",
+    author: "Michelle Obama",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "Spread love everywhere you go.",
+    author: "Mother Teresa",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "Do not allow people to dim your shine because they are blinded. Tell them to put some sunglasses on.",
+    author: "Lady Gaga",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "If you make your internal life a priority, then everything else you need on the outside will be given to you and it will be extremely clear what the next step is.",
+    author: "Gabrielle Bernstein",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "You don't always need a plan. Sometimes you just need to breathe, trust, let go and see what happens.",
+    author: "Mandy Hale",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "You can be everything. You can be the infinite amount of things that people are.",
+    author: "Kesha",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "What lies behind you and what lies in front of you, pales in comparison to what lies inside of you.",
+    author: "Ralph Waldo Emerson",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "I want to be in the arena. I want to be brave with my life. And when we make the choice to dare greatly, we sign up to get our asses kicked. We can choose courage or we can choose comfort, but we can't have both. Not at the same time.",
+    author: "Brene Brown",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "I'm going to be gone one day, and I have to accept that tomorrow isn't promised. Am I OK with how I’m living today? It's the only thing I can help. If I didn't have another one, what have I done with all my todays? Am I doing a good job?",
+    author: "Hayley Williams",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "I am experienced enough to do this. I am knowledgeable enough to do this. I am prepared enough to do this. I am mature enough to do this. I am brave enough to do this.",
+    author: "Alexandria Ocasio-Cortez in Knock Down The House",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "Belief creates the actual fact.",
+    author: "William James",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "No matter what people tell you, words and ideas can change the world.",
+    author: "Robin Williams as John Keating in Dead Poets Society",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "I'm not going to continue knocking that old door that doesn't open for me. I'm going to create my own door and walk through that.",
+    author: "Ava DuVernay",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "It is during our darkest moments that we must focus to see the light.",
+    author: "Aristotle",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "Not having the best situation, but seeing the best in your situation is the key to happiness.",
+    author: "Marie Forleo",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "Weaknesses are just strengths in the wrong environment.",
+    author: "Marianne Cantwell",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "Just don't give up trying to do what you really want to do. Where there is love and inspiration, I don't think you can go wrong.",
+    author: "Ella Fitzgerald",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "Silence is the last thing the world will ever hear from me.",
+    author: "Marlee Matlin",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "In a gentle way, you can shake the world.",
+    author: "Mahatma Gandhi",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "Learning how to be still, to really be still and let life happen—that stillness becomes a radiance.",
+    author: "Morgan Freeman",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "Everyone has inside of him a piece of good news. The good news is that you don't know how great you can be! How much you can love! What you can accomplish! And what your potential is!",
+    author: "Anne Frank",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "All you need is the plan, the road map, and the courage to press on to your destination.",
+    author: "Earl Nightingale",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "I care about decency and humanity and kindness. Kindness today is an act of rebellion.",
+    author: "Pink",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "If you have good thoughts they will shine out of your face like sunbeams and you will always look lovely.",
+    author: "Roald Dahl",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  {
+    text: "Try to be a rainbow in someone's cloud",
+    author: "Maya Angelou",
+    category: categories.motivation && categories.inspiration,
+    tags: ["motivation", "motivate"],
+  },
+  // 40 Motiviation Quotes - https://parade.com/973277/jessicasager/inspirational-quotes/
+  /**
+   * Happy Quotes
+   */
   {
     text: "I will never stop trying. Because when you find the one, you never give up.",
     author: "Crazy Stupid Love",
@@ -187,6 +437,9 @@ const quote: Quote[] = [
     category: categories.happy,
     tags: ["happy", "love"],
   },
+  /**
+   * Love Quotes
+   */
   {
     text: "Thinking of you keeps me awake. Dreaming of you keeps me asleep. Being with you keeps me alive.",
     author: "Unknown",
@@ -925,6 +1178,9 @@ const quote: Quote[] = [
     category: categories.love,
     tags: ["love", "romance"],
   },
+  /**
+   * Sad Quotes
+   */
   {
     text: "Our greatest joy and our greatest pain come in our relationships with others.",
     author: "Stephen R. Covey",
