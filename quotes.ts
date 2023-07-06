61611907d7acc464fb7366ab0d1a3ad93c69c083
Ducko <micasniebuhr@gmail.com>
2023-07-06 22:37:46 +0200

    Update quotes.ts

--- quotes.ts
+++ quotes.ts
@@ -27,10 +27,245 @@ export const quotes: Quote[] = [
    * Happy Quotes
    */
   {
-    author: "",
-    quote: "",
+    author: "Rosamunde Pilcher, 'The Shell Seekers'",
+    quote:
+      "Happiness is making the most of what you have, and riches is making the most of what you’ve got.",
     category: Categories.happy,
   },
+  {
+    author: "Dolly Parton",
+    quote:
+      "The way I see it, if you want the rainbow, you gotta put up with the rain!",
+    category: Categories.happy,
+  },
+  {
+    author: "Bob Ross",
+    quote: "We don’t make mistakes, just happy little accidents.",
+    category: Categories.happy,
+  },
+  {
+    author: "Billy Ivey",
+    quote:
+      "Misery might love company, but so does joy. And joy throws much better parties.",
+    category: Categories.happy,
+  },
+  {
+    author: "John Harrigan",
+    quote: "Happiness held is the seed; happiness shared is the flower.",
+    category: Categories.happy,
+  },
+  {
+    author: "Richard Wagner",
+    quote: "Joy is not in things; it is in us.",
+    category: Categories.happy,
+  },
+  {
+    author: "Sumit Sharma",
+    quote: "A Night can NEVER defeat the Sunrise.",
+    category: Categories.happy,
+  },
+  {
+    author: "Topher Kearby",
+    quote:
+      "It’s not about being happy all the time, or being sure of all your choices. It’s about knowing that life is precious, even when it’s tough.",
+    category: Categories.happy,
+  },
+  {
+    author: "Buddha",
+    quote: "There is no path to happiness; happiness is the path.",
+    category: Categories.happy,
+  },
+  {
+    author: "Bobby McFerrin",
+    quote: "Don't worry. Be happy.",
+    category: Categories.happy,
+  },
+  {
+    author: "Voltaire",
+    quote: "I have chosen to be happy because it's good for my health.",
+    category: Categories.happy,
+  },
+  {
+    author: "Buddha",
+    quote: "Happiness is a journey, not a destination.",
+    category: Categories.happy,
+  },
+  {
+    author: "Gabriel García Márquez",
+    quote: "No medicine cures what happiness cannot.",
+    category: Categories.happy,
+  },
+  {
+    author: "Tom Bodett",
+    quote:
+      "They say a person needs just three things to be truly happy in this world: Someone to love, something to do, and something to hope for.",
+    category: Categories.happy,
+  },
+  {
+    author: "Marcel Proust",
+    quote:
+      "Let us be grateful to the people who make us happy; they are the charming gardeners who make our souls blossom.",
+    category: Categories.happy,
+  },
+  {
+    author: "Charles M. Schulz",
+    quote: "Happiness is a warm puppy.",
+    category: Categories.happy,
+  },
+  {
+    author: "Stephen Chbosky, 'The Perks of Being a Wallflower'",
+    quote:
+      "There's nothing like deep breaths after laughing that hard. Nothing in the world like a sore stomach for the right reasons.",
+    category: Categories.happy,
+  },
+  {
+    author: "Mark Twain",
+    quote: "Sanity and happiness are an impossible combination.",
+    category: Categories.happy,
+  },
+  {
+    author: "Jonathan Safran Foer",
+    quote:
+      "You cannot protect yourself from sadness without protecting yourself from happiness.",
+    category: Categories.happy,
+  },
+  {
+    author: "Chuck Palahniuk, 'Diary'",
+    quote:
+      "It's so hard to forget pain, but it's even harder to remember sweetness. We have no scar to show for happiness. We learn so little from peace.",
+    category: Categories.happy,
+  },
+  {
+    author: "Roy T. Bennett",
+    quote:
+      "If you want to be happy, do not dwell in the past, do not worry about the future, focus on living fully in the present.",
+    category: Categories.happy,
+  },
+  {
+    author: "Dale Carnegie",
+    quote:
+      "It isn't what you have or who you are or where you are or what you are doing that makes you happy or unhappy. It is what you think about it.",
+    category: Categories.happy,
+  },
+  {
+    author: "Walt Disney",
+    quote:
+      "Happiness is a state of mind. It's just according to the way you look at things.",
+    category: Categories.happy,
+  },
+  {
+    author: "Anne Frank",
+    quote: "Think of all the beauty still left around you and be happy.",
+    category: Categories.happy,
+  },
+  {
+    author: "Eleanor Roosevelt",
+    quote: "Happiness is not a goal... it's a by-product of a life well lived.",
+    category: Categories.happy,
+  },
+  {
+    author: "Mark Twain",
+    quote: "The best way to cheer yourself is to try to cheer someone else up.",
+    category: Categories.happy,
+  },
+  {
+    author: "Dr. Seuss",
+    quote: "Don't cry because it's over, smile because it happened.",
+    category: Categories.happy,
+  },
+  {
+    author: "Aristotle",
+    quote: "Happiness depends upon ourselves.",
+    category: Categories.happy,
+  },
+  {
+    author: "Albert Einstein",
+    quote:
+      "A calm and modest life brings more happiness than the pursuit of success combined with constant restlessness.",
+    category: Categories.happy,
+  },
+  {
+    author: "Mother Teresa",
+    quote:
+      "If you find serenity and happiness, some may be jealous. Be happy anyway.",
+    category: Categories.happy,
+  },
+  {
+    author: "Lily Pulitzer",
+    quote: "Being happy never goes out of style.",
+    category: Categories.happy,
+  },
+  {
+    author: "Carrie Underwood",
+    quote:
+      "Every day is a new day, and you'll never be able to find happiness if you don't move on.",
+    category: Categories.happy,
+  },
+  {
+    author: "Ralph Waldo Emerson",
+    quote:
+      "For every minute you are angry you lose sixty seconds of happiness.",
+    category: Categories.happy,
+  },
+  {
+    author: "Benjamin Franklin",
+    quote:
+      "Happiness consists more in small conveniences or pleasures that occur every day, than in great pieces of good fortune that happen but seldom to a man in the course of his life.",
+    category: Categories.happy,
+  },
+  {
+    author: "Roy T. Bennett",
+    quote:
+      "Do not set aside your happiness. Do not wait to be happy in the future. The best time to be happy is always now.",
+    category: Categories.happy,
+  },
+  {
+    author: "Ellen DeGeneres",
+    quote:
+      "The thing everyone should realize is that the key to happiness is being happy by yourself and for yourself.",
+    category: Categories.happy,
+  },
+  {
+    author: "Muhammad Ali",
+    quote:
+      "Others may know pleasure, but pleasure is not happiness. It has no more importance than a shadow following a man.",
+    category: Categories.happy,
+  },
+  {
+    author: "John Lennon",
+    quote:
+      "Count your age by friends, not years. Count your life by smiles, not tears.",
+    category: Categories.happy,
+  },
+  {
+    author: "Dalai Lama",
+    quote:
+      "Happiness is not something readymade. It comes from your own actions.",
+    category: Categories.happy,
+  },
+  {
+    author: "Audrey Hepburn",
+    quote:
+      "The most important thing is to enjoy your life—to be happy—it's all that matters.",
+    category: Categories.happy,
+  },
+  {
+    author: "Christian Dior",
+    quote:
+      "Happiness is the secret to all beauty. There is no beauty without happiness.",
+    category: Categories.happy,
+  },
+  {
+    author: "Mahatma Gandhi",
+    quote:
+      "Happiness is when what you think, what you say, and what you do are in harmony.",
+    category: Categories.happy,
+  },
+  // {
+  //   author: "",
+  //   quote: "",
+  //   category: Categories.happy,
+  // },
   /**
    * inspiration Quotes
    */
@@ -78,11 +313,10 @@ export const quotes: Quote[] = [
     author: "",
     quote: "",
     category: Categories.success,
-  }
+  },
   /**
    * Sisdom Quotes
-   */,
-  {
+   */ {
     author: "",
     quote: "",
     category: Categories.wisdom,
