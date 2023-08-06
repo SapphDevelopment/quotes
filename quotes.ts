48ee14b9752b6423f7fb6d45186b89e04155ce91
Ducko <micasniebuhr@gmail.com>
2023-08-06 22:48:49 +0200

    Update quotes.ts
    fix(quotes): Fixed empty quote objects in success and wisdom (Added them)

--- quotes.ts
+++ quotes.ts
@@ -315,12 +315,6 @@ export const quotes: Quote[] = [
     category: Categories.inspiration,
   },
   {
-    author: "Winston Churchill",
-    quote:
-      "Success is not final, failure is not fatal: it is the courage to continue that counts.",
-    category: Categories.inspiration,
-  },
-  {
     author: "Oprah Winfrey",
     quote:
       "You define your own life. Don't let other people write your script.",
@@ -1222,117 +1216,140 @@ export const quotes: Quote[] = [
     category: Categories.motivation,
   },
   {
-    quote: "I don’t need to be motivated because I’m a disciplined person.\nIf I allocate X amount of time to do something, I’m going to do exactly that, with complete and absolute vigor for the allocated amount of time.",
+    quote:
+      "I don’t need to be motivated because I’m a disciplined person.\nIf I allocate X amount of time to do something, I’m going to do exactly that, with complete and absolute vigor for the allocated amount of time.",
     author: "Andrew Tate",
     category: Categories.motivation,
   },
   {
-    quote: "Every action you take is molding who you are as a person.\n\nEvery time that you sleep in.\nEvery time you are undisciplined, you are training yourself that it’s okay.\nA downward spiral towards mediocrity.",
+    quote:
+      "Every action you take is molding who you are as a person.\n\nEvery time that you sleep in.\nEvery time you are undisciplined, you are training yourself that it’s okay.\nA downward spiral towards mediocrity.",
     author: "Andrew Tate",
     category: Categories.motivation,
   },
   {
-    quote: "You are exactly where you deserve to be.\nChange who you are and you will change how you live.",
+    quote:
+      "You are exactly where you deserve to be.\nChange who you are and you will change how you live.",
     author: "Andrew Tate",
     category: Categories.motivation,
   },
   {
-    quote: "If I’m not doing something which is either extremely difficult or extremely stressful I am in a perpetual state of crippling boredom.\nTherefore I constantly seek difficulty to solve and forever succeed.",
+    quote:
+      "If I’m not doing something which is either extremely difficult or extremely stressful I am in a perpetual state of crippling boredom.\nTherefore I constantly seek difficulty to solve and forever succeed.",
     author: "Andrew Tate",
     category: Categories.motivation,
   },
   {
-    quote: "Men are not designed to be comfortable. They want to achieve. They want to feel pain and suffering. They want to conquer something.",
+    quote:
+      "Men are not designed to be comfortable. They want to achieve. They want to feel pain and suffering. They want to conquer something.",
     author: "Andrew Tate",
     category: Categories.motivation,
   },
   {
-    quote: "The person who goes to the gym every day regardless of how they feel will always beat the person who goes to the gym when they feel like going to the gym.",
+    quote:
+      "The person who goes to the gym every day regardless of how they feel will always beat the person who goes to the gym when they feel like going to the gym.",
     author: "Andrew Tate",
     category: Categories.motivation,
   },
   {
-    quote: "The immovable object beats the unstoppable force. The immovable object requires less energy. Efficiency always wins.",
+    quote:
+      "The immovable object beats the unstoppable force. The immovable object requires less energy. Efficiency always wins.",
     author: "Andrew Tate",
     category: Categories.motivation,
   },
   {
-    quote: "Emotional control isn’t a lack of emotion, it’s a necessary function of maturity.",
+    quote:
+      "Emotional control isn’t a lack of emotion, it’s a necessary function of maturity.",
     author: "Andrew Tate",
     category: Categories.motivation,
   },
   {
-    quote: "Value is linked to difficulty. If you want something that is valuable, you need something which is difficult to obtain. If it was easy, you wouldn't even want it. So why are you complaining that it's hard?",
+    quote:
+      "Value is linked to difficulty. If you want something that is valuable, you need something which is difficult to obtain. If it was easy, you wouldn't even want it. So why are you complaining that it's hard?",
     author: "Andrew Tate",
     category: Categories.motivation,
   },
   {
-    quote: "I could never stop fighting even if I lost 100 fights because I’m not built to live a normal existence.",
+    quote:
+      "I could never stop fighting even if I lost 100 fights because I’m not built to live a normal existence.",
     author: "Andrew Tate",
     category: Categories.motivation,
   },
   {
-    quote: "For 12 years I trained 5 hours per day, six days per week, and I was motivated to train probably 25% of the time. The rest of the time I went because I am disciplined.",
+    quote:
+      "For 12 years I trained 5 hours per day, six days per week, and I was motivated to train probably 25% of the time. The rest of the time I went because I am disciplined.",
     author: "Andrew Tate",
     category: Categories.motivation,
   },
   {
-    quote: "The average man doesn’t try very hard. So if you try very hard, it’s impossible to be average.",
+    quote:
+      "The average man doesn’t try very hard. So if you try very hard, it’s impossible to be average.",
     author: "Andrew Tate",
     category: Categories.motivation,
   },
   {
-    quote: "You’re going to have to work when you don’t feel like working. That’s how it’s going to have to be, or you’re never going to be important.",
+    quote:
+      "You’re going to have to work when you don’t feel like working. That’s how it’s going to have to be, or you’re never going to be important.",
     author: "Andrew Tate",
     category: Categories.motivation,
   },
   {
-    quote: "Sadness is a warning. You feel it for a reason. Your mind is telling you that you need to work harder.",
+    quote:
+      "Sadness is a warning. You feel it for a reason. Your mind is telling you that you need to work harder.",
     author: "Andrew Tate",
     category: Categories.motivation,
   },
   {
-    quote: "A good plan today is better than a perfect plan tomorrow. You’ve gotta do something and you’ve gotta do something fast. You just sitting around talking about it and hoping for things to get better is how you LOSE.",
+    quote:
+      "A good plan today is better than a perfect plan tomorrow. You’ve gotta do something and you’ve gotta do something fast. You just sitting around talking about it and hoping for things to get better is how you LOSE.",
     author: "Andrew Tate",
     category: Categories.motivation,
   },
   {
-    quote: "Most people see other people with things they want and don’t do the second half. They don’t try to work out how they got that thing.",
+    quote:
+      "Most people see other people with things they want and don’t do the second half. They don’t try to work out how they got that thing.",
     author: "Andrew Tate",
     category: Categories.motivation,
   },
   {
-    quote: "The temporary satisfaction of quitting is outweighed by the eternal suffering of being a nobody.",
+    quote:
+      "The temporary satisfaction of quitting is outweighed by the eternal suffering of being a nobody.",
     author: "Andrew Tate",
     category: Categories.motivation,
   },
   {
-    quote: "Close your eyes.\nFocus on making yourself feel excited, powerful.\nImagine destroying every goal with ease.\nSoon - a smile will appear.\nNow open your eyes.\nGet to work.",
+    quote:
+      "Close your eyes.\nFocus on making yourself feel excited, powerful.\nImagine destroying every goal with ease.\nSoon - a smile will appear.\nNow open your eyes.\nGet to work.",
     author: "Andrew Tate",
     category: Categories.motivation,
   },
   {
-    quote: "The amount of stress you can tolerate while remaining effective is directly correlated to the level of success you will enjoy.",
+    quote:
+      "The amount of stress you can tolerate while remaining effective is directly correlated to the level of success you will enjoy.",
     author: "Andrew Tate",
     category: Categories.motivation,
   },
   {
-    quote: "Don’t listen to the advice of people who are living lives you don’t want to live.",
+    quote:
+      "Don’t listen to the advice of people who are living lives you don’t want to live.",
     author: "Andrew Tate",
     category: Categories.motivation,
   },
   {
-    quote: "When’s the last time you prevented the negative instead of dealing with it once it's happened?",
+    quote:
+      "When’s the last time you prevented the negative instead of dealing with it once it's happened?",
     author: "Andrew Tate",
     category: Categories.motivation,
   },
   {
-    quote: "Discipline is the key to success.\nAbsolutely is.\nIf you cannot force yourself to do something that you do not want to do, how are you ever gonna put yourself through the suffering required for greatness?",
+    quote:
+      "Discipline is the key to success.\nAbsolutely is.\nIf you cannot force yourself to do something that you do not want to do, how are you ever gonna put yourself through the suffering required for greatness?",
     author: "Andrew Tate",
     category: Categories.motivation,
   },
   {
-    quote: "I don’t think there’s a person on this planet who has a 100% approval rate.",
+    quote:
+      "I don’t think there’s a person on this planet who has a 100% approval rate.",
     author: "Andrew Tate",
     category: Categories.motivation,
   },
@@ -2539,15 +2556,150 @@ export const quotes: Quote[] = [
    * Success Quotes
    */
   {
-    author: "",
-    quote: "",
+    author: "Benjamin Franklin",
+    quote:
+      "Without continual growth and progress, such words as improvement, achievement, and success have no meaning.",
+    category: Categories.success,
+  },
+  {
+    author: "Albert Einstein",
+    quote:
+      "Try not to become a man of success, but rather try to become a man of value.",
+    category: Categories.success,
+  },
+  {
+    author: "Bill Gates",
+    quote:
+      "Success is a lousy teacher. It seduces smart people into thinking they can't lose.",
+    category: Categories.success,
+  },
+  {
+    author: "Doug Larson",
+    quote:
+      "A true friend is one who overlooks your failures and tolerates your success.",
+    category: Categories.success,
+  },
+  {
+    author: "Bill Bradley",
+    quote:
+      "Ambition is the path to success. Persistence is the vehicle you arrive in.",
+    category: Categories.success,
+  },
+  {
+    author: "Bobby Unser",
+    quote: "Success is where preparation and opportunity meet.",
+    category: Categories.success,
+  },
+  {
+    author: "Franklin D. Roosevelt",
+    quote:
+      "Happiness lies in the joy of achievement and the thrill of creative effort.",
+    category: Categories.success,
+  },
+  {
+    author: "Colin Powell",
+    quote:
+      "Success is the result of perfection, hard work, learning from failure, loyalty, and persistence.",
+    category: Categories.success,
+  },
+  {
+    author: "Henry Ford",
+    quote:
+      "Coming together is a beginning; keeping together is progress; working together is success.",
+    category: Categories.success,
+  },
+  {
+    author: "Virat Kohli",
+    quote: "Self-belief and hard work will always earn you success.",
+    category: Categories.success,
+  },
+  {
+    author: "Patricia Cornwell",
+    quote: "Survival was my only hope, success my only revenge.",
+    category: Categories.success,
+  },
+  {
+    author: "Winston Churchill",
+    quote:
+      "Success consists of going from failure to failure without loss of enthusiasm.",
+    category: Categories.success,
+  },
+  {
+    author: "John C. Maxwell",
+    quote: "The secret of your success is determined by your daily agenda.",
+    category: Categories.success,
+  },
+  {
+    author: "Stephen Hawking",
+    quote:
+      "However difficult life may seem, there is always something you can do and succeed at.",
+    category: Categories.success,
+  },
+  {
+    author: "Shiv Khera",
+    quote:
+      "Your positive action combined with positive thinking results in success.",
+    category: Categories.success,
+  },
+  {
+    author: "Winston Churchill",
+    quote:
+      "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+    category: Categories.success,
+  },
+  {
+    author: "Napoleon Hill",
+    quote:
+      "Patience, persistence and perspiration make an unbeatable combination for success.",
+    category: Categories.success,
+  },
+  {
+    author: "Wayne Huizenga",
+    quote:
+      "Some people dream of success, while other people get up every morning and make it happen.",
+    category: Categories.success,
+  },
+  {
+    author: "Swami Sivananda",
+    quote:
+      "Put your heart, mind, and soul into even your smallest acts. This is the secret of success.",
     category: Categories.success,
   },
   /**
    * Wisdom Quotes
-   */ {
-    author: "",
-    quote: "",
+   */
+  {
+    author: "Aristotle",
+    quote:
+      "It is the mark of an educated mind to be able to entertain a thought without accepting it.",
+    category: Categories.wisdom,
+  },
+  {
+    author: "Confucius",
+    quote:
+      "The man who asks a question is a fool for a minute, the man who does not ask is a fool for life.",
+    category: Categories.wisdom,
+  },
+  {
+    author: "Lao Tzu",
+    quote:
+      "Do the difficult things while they are easy and do the great things while they are small. A journey of a thousand miles must begin with a single step.",
+    category: Categories.wisdom,
+  },
+  {
+    author: "Seneca",
+    quote:
+      "It is not because things are difficult that we do not dare; it is because we do not dare that things are difficult.",
+    category: Categories.wisdom,
+  },
+  {
+    author: "Socrates",
+    quote: "The only true wisdom is in knowing you know nothing.",
+    category: Categories.wisdom,
+  },
+  {
+    author: "Plato",
+    quote: "The greatest wealth is to live content with little.",
     category: Categories.wisdom,
   },
 ];
